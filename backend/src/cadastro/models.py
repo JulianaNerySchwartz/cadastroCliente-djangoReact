37fe35a05a9bcf6e60e0891ca98c5a49016248eb
Juliana Nery Schwartz <juliana.schwartz@hotmail.com>
2021-06-07 21:14:39 -0300

    Criação do Model 'Cadastro'

--- backend/src/cadastro/models.py
+++ backend/src/cadastro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Cadastro(models.Model):
+    name = models.CharField(max_length=120)
+    cpf = models.CharField(max_length=15)
+    email = models.CharField(max_length=120)
+    gender_choice = [('F', 'Feminino'), ('M', 'Masculino'), ('O', 'Outros')]
+    gender = models.CharField(max_length=30, null=True,
+                              blank=True, choices=gender_choice)
+
+    def __str__(self):
+        return self.name
